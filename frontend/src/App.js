042b6f6e721711441f47ba590a320daf045f6465
Jupiter Santana <lreye089@fiu.edu>
2022-05-21 13:24:29 -0400

    minor updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,29 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import React from 'react';
+import React from "react";
 
-import {Home} from './Home';
-import {Warehouse} from './Warehouse';
-import {Inventory} from './Inventory';
-import {Navigation} from './Navigation';
+import { Home } from "./Home";
+import { Warehouse } from "./Warehouse";
+import { Inventory } from "./Inventory";
+import { Navigation } from "./Navigation";
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="container">
-     <h3 className="m-3 d-flex justify-content-center">
-       React JS Tutorial
-     </h3>
+      <div className="container">
+        <h3 className="m-3 d-flex justify-content-center">
+          Inventory Management
+        </h3>
 
-     <Navigation/>
-    
-{/* This one works for React 18 */}
-    <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route path='/inventory' element={<Inventory/>}/>
-      {/* <Route path='/employee' element={<Employee/>}/> */}
-      <Route path='/warehouse' element={<Warehouse/>}/>
-      
-    </Routes>
-
-{/* Bottom works for OLD REACT */}
-     {/* <Routes>
-       <Route path='/' component={Home} exact/>
-       <Route path='/department' component={Department}/>
-       <Route path='/employee' component={Employee}/>
-     </Routes> */}
-
-
-    </div>
+        <Navigation />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/warehouse" element={<Warehouse />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- frontend/src/Inventory.js
+++ frontend/src/Inventory.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 
 export class Inventory extends Component {
   render() {
-    return (
-      <div className="mt-5 d-flex justify-content-left">INVENTORY PAGE</div>
-    );
+    return <div className="mt-5 d-flex justify-content-left">INVENTORY</div>;
   }
 }
